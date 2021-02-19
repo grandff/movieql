2d7244431aa8369b9caa5a2b627e7ccefe3244ad
Yura Ko <dsaqaz421@naver.com>
2021-02-19 13:04:55 +0900

    0.7 Extending the Schema part Two

---
+++ graphql/db.js
@@ -0,0 +1,43 @@
+export const people = [
+    {
+        id : "0",
+        name : "Nicolas",
+        age : 18,
+        gender : "female"
+    },
+    {
+        id : "1",
+        name : "Nick",
+        age : 23,
+        gender : "female"
+    },
+    {
+        id : "2",
+        name : "Cat",
+        age : 31,
+        gender : "male"
+    },
+    {
+        id : "3",
+        name : "Shit",
+        age : 55,
+        gender : "male"
+    },
+    {
+        id : "4",
+        name : "Fuck",
+        age : 71,
+        gender : "female"
+    },
+    {
+        id : "5",
+        name : "Kim",
+        age : 29,
+        gender : "male"
+    }    
+];
+
+export const getById = id => {
+    const filteredPeole = movies.filter(person => people.id === id);
+    return filteredPeople[0];
+}
